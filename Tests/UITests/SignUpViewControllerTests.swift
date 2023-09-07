e8ac8b1eb80a98dc1da636047a8e7aafc63fff02
Rafael Batista <maranata_rafa@hotmail.com>
2023-09-07 23:07:12 +0100

    add test to guarantee that SignUpViewController implemets loadingViewProtocol

--- Tests/UITests/SignUpViewControllerTests.swift
+++ Tests/UITests/SignUpViewControllerTests.swift
@@ -14,4 +14,13 @@ final class SignUpViewControllerTests: XCTestCase {
         //assert
         expect(should: sut.loadingIndicator?.isAnimating, beEqual: false)
     }
+
+    func test_givenSignUpPage_whenInstanced_thenEnsureExtendsLoadingViewProtocol(){
+        //arrange
+        let sb = UIStoryboard(name: "SignUp", bundle: Bundle(for: SignUpViewController.self))
+        let sut = sb.instantiateViewController(identifier: "SignUpViewController") as! SignUpViewController
+
+        //act & assert
+        expect(shouldNotBeNil: sut as LoadingViewProtocol)
+    }
 }
